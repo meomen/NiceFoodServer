56c968a3fe13905039cc7f53ff7219c135f50a99
Vũ Đức Minh <43166943+meomen@users.noreply.github.com>
2020-02-06 20:56:23 +0700

    Authentication

---
+++ app/src/main/java/com/vuducminh/nicefoodserver/common/Common.java
@@ -0,0 +1,7 @@
+package com.vuducminh.nicefoodserver.common;
+
+import com.vuducminh.nicefoodserver.model.ServerUserModel;
+
+public class Common {
+    public static ServerUserModel currentServerUser;
+}
---
+++ app/src/main/java/com/vuducminh/nicefoodserver/common/CommonAgr.java
@@ -0,0 +1,5 @@
+package com.vuducminh.nicefoodserver.common;
+
+public class CommonAgr {
+    public static final String SERVER_REF = "Server";
+}
