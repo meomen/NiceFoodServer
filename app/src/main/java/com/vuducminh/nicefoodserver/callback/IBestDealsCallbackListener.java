3e383947342fa63f8ba27f9c5846949875e6a321
Vũ Đức Minh <43166943+meomen@users.noreply.github.com>
2020-06-28 12:03:41 +0000

    Update IBestDealsCallbackListener.java

--- app/src/main/java/com/vuducminh/nicefoodserver/callback/IBestDealsCallbackListener.java
+++ app/src/main/java/com/vuducminh/nicefoodserver/callback/IBestDealsCallbackListener.java
@@ -3,7 +3,7 @@ package com.vuducminh.nicefoodserver.callback;
 import com.vuducminh.nicefoodserver.model.BestDealsModel;
 
 import java.util.List;
-
+ //lắng nghe sự kiện item món ăn đề xuất(Best Deal) được chọn
 public interface IBestDealsCallbackListener {
     void onListBestDealsLoadSuccess(List<BestDealsModel> bestDealsModels);
     void onListBestDealsLoadFailed(String message);
